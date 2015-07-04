31a04f9b957a1b9a06de9c1bbc22e448dfc83572
vieira <vieira@yubo.be>
2015-07-04 16:47:34 +0000

    Simpler deserializing logic for config file
    By using JSON dictionary instead of array of repositories
    we are able to directly deserialize to the internal data structure.

--- src/config/config.go
+++ src/config/config.go
@@ -7,7 +7,6 @@ import (
 )
 
 type Repository struct {
-    Url             string
     Ref             string
     Path            string
     Owner           *Owner
@@ -18,28 +17,26 @@ type Owner struct {
     Email           string
 }
 
+type Slack struct {
+    Username        string
+    Webhook         string
+    Icon            string
+    Channel         string
+}
+
 type Config struct {
     Listen          string
+    Slack           *Slack
     Repositories    map[string] Repository
 }
 
 func Read(fileName string) *Config {
     var c Config
-    jc := struct {
-        Listen string
-        Repositories []Repository
-    }{}
 
     if data, err := ioutil.ReadFile(fileName); err != nil {
         log.Panicln(err)
-    } else if err := json.Unmarshal(data, &jc); err != nil {
+    } else if err := json.Unmarshal(data, &c); err != nil {
         log.Panicln(err)
-    }
-
-    c.Listen = jc.Listen
-    c.Repositories = make(map[string] Repository, len(jc.Repositories))
-    for _, repo := range jc.Repositories {
-        c.Repositories[repo.Url] = repo
     }
 
     return &c
